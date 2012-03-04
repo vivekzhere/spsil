055b28c8e5457c6c9429087bdd6f8a9f108bbf90
Vivek Anand T Kallampally <vivekzhere@gmail.com>
2012-03-05 00:23:54 +0530

    lots of changes, segmentation fault

--- spsil.h
+++ spsil.h
@@ -1,4 +1,7 @@
+#include<string.h>
+#define LEGAL 0
 extern int linecount;
+int flag_alias=LEGAL;
 struct define
 {
 	char *name;
@@ -20,6 +23,167 @@ struct tree
 	struct define *entry;
 	struct tree *ptr1,*ptr2,*ptr3;
 };
-struct define *root=NULL;
+struct define *define_root=NULL;
 FILE *fp;
-char table[8][30];
+char alias_table[8][30];
+
+struct define* lookup_constant(char *name)
+{
+	struct define *temp=define_root;	
+	while(temp!=NULL)
+	{
+		if(strcmp(name,temp->name)==0)
+			return temp;
+		temp=temp->next;
+	}
+	return NULL;
+}
+int lookup_alias(char *name)
+{
+	int i=0;
+	while(i<8)
+	{
+		if(strcmp(alias_table[i],name)==0)
+			return(i+8);
+		i++;
+	}
+	return(-1);
+}
+void insert_constant(char *name,int value)
+{
+	struct define * temp;
+	temp=lookup_constant(name);
+	if(temp==NULL)
+	{
+		temp=malloc(sizeof(struct define));
+		strcpy(temp->name,name);
+		temp->value=value;
+		temp->next=define_root;
+		define_root=temp;
+	}
+	else
+	{
+		printf("\n%d: Multiple definition of symbolic contant %s !!\n",linecount,name);
+		exit(0);
+	}
+} 
+void insert_alias(char *name,int value)
+{
+	struct define * temp;
+	if(flag_alias!=LEGAL)
+	{
+		printf("\n%d: Aliasing cannot be done within if and while!!\n",linecount);
+		exit(0);
+	}
+	if(!(value>=8 && value <=15))
+	{
+		printf("\n%d: Only kernel registers R8 to R15 can be aliased!!\n",linecount);
+		exit(0);
+	}	
+	temp=lookup_constant(name);
+	if(temp==NULL)
+		strcpy(alias_table[value-8],name);
+	else
+	{
+		printf("\n%d: Alias name %s already used as symbolic contant!!\n",linecount,name);
+		exit(0);
+	}
+}      
+void add_predefined_constants()
+{
+	struct define * temp;
+	char name[15];
+	strcpy(name,"SCRATCHPAD");
+	if(lookup_constant(name)==NULL)
+		insert_constant(name,256);
+		
+	strcpy(name,"PAGE_TABLE");
+	if(lookup_constant(name)==NULL)
+		insert_constant(name,512);
+		
+	strcpy(name,"MEM LIST");
+	if(lookup_constant(name)==NULL)
+		insert_constant(name,576);
+		
+	strcpy(name,"FILE TABLE");
+	if(lookup_constant(name)==NULL)
+		insert_constant(name,640);
+		
+	strcpy(name,"READY LIST");
+	if(lookup_constant(name)==NULL)
+		insert_constant(name,736);
+		
+	strcpy(name,"PROC TABLE");
+	if(lookup_constant(name)==NULL)
+		insert_constant(name,767);
+		
+	strcpy(name,"FAT");
+	if(lookup_constant(name)==NULL)
+		insert_constant(name,1024);
+		
+	strcpy(name,"DISK LIST");
+	if(lookup_constant(name)==NULL)
+		insert_constant(name,1536);
+	strcpy(name,"USER PROG");
+	if(lookup_constant(name)==NULL)
+		insert_constant(name,1792);
+		
+	strcpy(name,"INTERRUPT");
+	if(lookup_constant(name)==NULL)
+		insert_constant(name,13824);	
+}
+
+struct tree * create_nonterm_node(char *name,struct tree *a,struct tree *b)
+{
+	struct tree *temp=malloc(sizeof(struct tree));
+	temp->nodetype='n';
+	temp->name=name;
+	temp->entry=NULL;
+	temp->ptr1=a;
+	temp->ptr2=b;
+	temp->ptr3=NULL;
+	return temp;
+}
+struct tree * create_tree(struct tree *a,struct tree *b,struct tree *c,struct tree *d)
+{
+	printf("hai");
+	a->ptr1=b;
+	a->ptr2=c;
+	a->ptr3=d;	
+}
+struct tree * substitute_id(struct tree *id)
+{
+	struct define *temp;
+	int n;
+	temp=lookup_constant(id->name);
+	if(temp!=NULL)
+	{
+		id->nodetype='c';
+		id->name=NULL;
+		id->value=temp->value;
+		return(id);
+	}
+	n=lookup_alias(id->name);
+	if(n<0)
+	{
+		printf("\n%d: Unknown identifier %s used!!\n",linecount,id->name);
+		exit(0);
+	}
+	id->nodetype='R';
+	id->name=NULL;
+	id->value=n;
+	return(id);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
--- spsil.y
+++ spsil.y
@@ -7,103 +7,117 @@
 {
 	struct tree *n;
 }
-%token ALIAS DEFINE DO ELSE ENDIF ENDWHILE IF IRETURN LOAD  STORE THEN WHILE PRINT REG NUM ASG ARITHOP1 ARITHOP2 RELOP LOGOP NOTOP ID
-%type<n> IF IRETURN LOAD STORE WHILE PRINT REG NUM ASSIGNOP ARITHOP1 ARITHOP2 RELOP LOGOP NOTOP ID STRING stmtlist stmt expr ids
+%token ALIAS DEFINE DO ELSE ENDIF ENDWHILE IF IRETURN LOAD  STORE THEN WHILE REG NUM ASSIGNOP ARITHOP1 ARITHOP2 RELOP LOGOP NOTOP ID
+%type<n> IF IRETURN LOAD STORE WHILE REG NUM ASSIGNOP ARITHOP1 ARITHOP2 RELOP LOGOP NOTOP ID stmtlist stmt expr ids ifpad whilepad
 %left LOGOP
 %left RELOP  
 %left ARITHOP1		// + and -
 %left ARITHOP2		// * , / and %
 %right NOTOP		// NOT Operator
-%left UMIN		// unary minus
 %%
-body:		definelist stmtlist			{
+body:		definelistpad stmtlist			{
 							}
 		;
-			
+
+definelistpad:	definelist				{
+								add_predefined_constants();
+							}
+		;
 definelist:						{
 							}
 		|definelist definestmt			{
 							}
 		;
 
-definestmt:	DEFINE STRING NUM ';'			{
+definestmt:	DEFINE ID NUM ';'			{
+								insert_constant($2->name,$3->value);
 							}
 		;
 
 stmtlist:	stmtlist stmt 				{
-								$$=NULL;
+								$$=create_nonterm_node("Body",$1,$2);
 							}
 		|stmt					{
-								$$=NULL;
+								$$=$1;
 							}
 		;
 
 stmt:		ids ASSIGNOP expr ';'	 		{
-								$$=NULL;
+								$$=create_tree($2,$1,$3,NULL);
 							}
-		|PRINT '(' expr ')' ';'			{
-								$$=NULL;
-							}			
-		|IF expr THEN stmtlist ENDIF ';'	{
-								$$=NULL;
+		|ifpad expr THEN stmtlist ENDIF ';'	{								
+								$$=create_tree($1,$2,$4,NULL);
+								flag_alias--;
 							}								
-		|IF expr THEN stmtlist
+		|ifpad expr THEN stmtlist
 		ELSE stmtlist ENDIF ';'			{	
+								$$=create_tree($1,$2,$4,$6);
+								flag_alias--;
+							}
+		|whilepad expr DO stmtlist ENDWHILE ';'	{
+								$$=create_tree($1,$2,$4,NULL);
+								flag_alias--;
+							}
+		|ALIAS ID REG ';'			{	
+								insert_alias($2->name,$3->value);
 								$$=NULL;
 							}
-		|WHILE expr DO stmtlist ENDWHILE ';'	{
-								$$=NULL;
-							}
-		|ALIAS REG ID ';'			{
-								$$=NULL;
-							}
-			;
+		;
 				
 expr:		expr ARITHOP1 expr			{
-								$$=NULL;
+								$$=create_tree($2,$1,$3,NULL);
 							}
 		|expr ARITHOP2 expr			{
-								$$=NULL;
+								$$=create_tree($2,$1,$3,NULL);
 							}
 		|expr RELOP expr 			{
-								$$=NULL;
+								$$=create_tree($2,$1,$3,NULL);
 							}
 		|expr LOGOP expr			{
-								$$=NULL;
+								$$=create_tree($2,$1,$3,NULL);
 							}
 		|NOTOP expr				{
-								$$=NULL;
+								$$=create_tree($1,$2,NULL,NULL);
 							}
 		|'('expr')'				{
 								$$=$2;
 							}
-		|ARITHOP1 expr	%prec UMIN		{
-								$$=NULL;
-							}
-		|NUM					{
+		|NUM					{	
 								$$=$1;
 							}
 		|ids					{
 								$$=$1;
 							}
 		;
+
+ifpad:		IF					{
+								flag_alias++;
+								$$=$1;
+							}
+		;
+
+whilepad:	WHILE					{
+								flag_alias++;
+								$$=$1;
+							}
+		;
 			
-ids:		ID					{
-								$$=NULL;
+ids:		ID					{							
+								$$=$1;//substitute_id($1);
 							}
 		|REG					{
-								$$=NULL;
+								$$=$1;
 							}
 		;
 %%
 int main (void)
 {	
-	fp=fopen("./sim/sim.asm","w");
-	fprintf(fp,"START\n");	
+	//fp=fopen("./sim/sim.asm","w");
+	//fprintf(fp,"START\n");	
 	return yyparse();
 }
 
 int yyerror (char *msg) 
 {
-	return fprintf (stderr, "YACC: %s\n", msg);
+	return fprintf (stderr, "%d: %s\n",linecount,msg);
 }
